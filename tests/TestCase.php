04f699a75caa5a8025d20c82eca1591584ecd219
Carsten Brandt <mail@cebe.cc>
2018-03-12 14:37:32 +0100

    added test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -119,4 +119,25 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
 
         return $db;
     }
+
+    /**
+     * Invokes a inaccessible method.
+     * @param $object
+     * @param $method
+     * @param array $args
+     * @param bool $revoke whether to make method inaccessible after execution
+     * @return mixed
+     */
+    protected function invokeMethod($object, $method, $args = [], $revoke = true)
+    {
+        $reflection = new \ReflectionObject($object);
+        $method = $reflection->getMethod($method);
+        $method->setAccessible(true);
+        $result = $method->invokeArgs($object, $args);
+        if ($revoke) {
+            $method->setAccessible(false);
+        }
+
+        return $result;
+    }
 }
