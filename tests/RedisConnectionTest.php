8b26f54f09fbbffde8940b9a9703fd3295e04afb
terabytesoftw <terabytesoftw@gmail.com>
2020-09-11 14:17:16 -0300

    Revert change RedisConnectionTest.php

--- tests/RedisConnectionTest.php
+++ tests/RedisConnectionTest.php
@@ -152,7 +152,7 @@ class ConnectionTest extends TestCase
         $db->configSet('timeout', 1);
         $db->on(Connection::EVENT_AFTER_OPEN, function() {
             // sleep 2 seconds after connect to make every command time out
-            sleep(4);
+            sleep(2);
         });
         $this->assertCount(3, $logger->messages, 'log of connection and init commands.');
 
