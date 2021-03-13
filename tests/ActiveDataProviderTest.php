9092631282458e9afc8f022f4df31f4326a82c65
Bizley <pawel@positive.codes>
2021-03-13 11:59:35 +0100

    Connection class

--- tests/ActiveDataProviderTest.php
+++ tests/ActiveDataProviderTest.php
@@ -3,10 +3,8 @@
 namespace yiiunit\extensions\redis;
 
 use yii\data\ActiveDataProvider;
-use yii\redis\ActiveQuery;
 use yiiunit\extensions\redis\data\ar\ActiveRecord;
 use yiiunit\extensions\redis\data\ar\Item;
-use yiiunit\framework\ar\ActiveRecordTestTrait;
 
 /**
  * @group redis
@@ -30,12 +28,14 @@ class ActiveDataProviderTest extends TestCase
     {
         $query = Item::find();
         $provider = new ActiveDataProvider([
+            'connectionClass' => 'yii\redis\Connection',
             'query' => $query,
         ]);
         $this->assertCount(2, $provider->getModels());
 
         $query = Item::find()->where(['category_id' => 1]);
         $provider = new ActiveDataProvider([
+            'connectionClass' => 'yii\redis\Connection',
             'query' => $query,
         ]);
         $this->assertCount(1, $provider->getModels());
